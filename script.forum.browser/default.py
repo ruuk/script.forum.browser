49f0a0c600f2a910f1370544f6cc02b166ee7f8e
ruuk <xbmc@2ndmind.net>
2012-06-10 17:59:12 +0000

    Added help for 'User Extra Info' option in posts and full post view

--- script.forum.browser/default.py
+++ script.forum.browser/default.py
@@ -21,7 +21,7 @@ __plugin__ = 'Forum Browser'
 __author__ = 'ruuk (Rick Phillips)'
 __url__ = 'http://code.google.com/p/forumbrowserxbmc/'
 __date__ = '03-29-2012'
-__version__ = '0.9.53'
+__version__ = '1.0.0'
 __addon__ = xbmcaddon.Addon(id='script.forum.browser')
 __language__ = __addon__.getLocalizedString
 
